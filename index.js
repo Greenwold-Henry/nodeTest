f6618c71ea5c0f6737c23423c6c3fcae17e4168f
monkhouse <henry.greenwold@gmail.com>
2017-12-06 05:08:38 +0200

    Factored asking into its own module

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi');
+const ask = require('./ask');
+main().then(() => ask.close());
+
+async function main() {
+
+    console.log('hi');
+    let ans = await(ask('How did it happen? '));
+    console.log(`Answer ${ans}`);
+
+}
